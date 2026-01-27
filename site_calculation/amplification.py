2b8e61d47dce5e9d052479f5ac975a628e8139e9
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2026-01-27 14:32:05 +1300

    docs(amplification): document site amplification models

--- site_calculation/amplification.py
+++ site_calculation/amplification.py
@@ -14,7 +14,47 @@ BAYLESS_ABRAHAMSON_2018_FREQUENCIES: FrequencyArray = (
     _utils._bayless_abrahamson_2018_frequencies()
 )
 
+
 def campbell_bozorgnia_2014(sites: pd.DataFrame) -> AmplificationArray:
+    """Site amplification factor based on the Campbell and Bozorgnia
+    2014 spectral acceleration ground motion model [0] with
+    modifications[1].
+
+    Parameters
+    ----------
+    sites : pd.DataFrame
+        DataFrame of sites to calculate amplification factors for.
+        Must contain all columns in `REQUIRED_COLUMNS`, at least
+        ``vs30`` (vs30 of the site), ``vs30_sim`` (vs30 of the
+        simulation, typically 500 m/s), and ``pga`` (peak ground
+        acceleration from the simulation).
+
+    Returns
+    -------
+    AmplificationArray
+        An array of shape ``(n_stations, n_frequencies)`` where
+        ``n_frequencies`` is the number of frequencies supported by
+        the model.
+
+    Notes
+    -----
+    Implementation details are in ``src-rust/campbell_bozorgnia_2014.rs``
+
+    See Also
+    --------
+    CAMPBELL_BOZORGNIA_2014_FREQUENCIES : Frequencies of the amplification values.
+
+    References
+    ----------
+    [0] Campbell KW, Bozorgnia Y. NGA-West2 Ground Motion Model for
+    the Average Horizontal Components of PGA, PGV, and 5% Damped
+    Linear Acceleration Response Spectra. Earthquake Spectra.
+    2014;30(3):1087-1115. doi:10.1193/062913EQS175M
+    [1] Kuncar F, Bradley BA, de la Torre CA, Rodriguez-Marek A, Zhu C,
+    Lee RL. Methods to account for shallow site effects in hybrid
+    broadband ground-motion simulations. Earthquake Spectra.
+    2025;41(2):1272-1313.
+    """
     column_diff = REQUIRED_COLUMNS - set(sites.columns)
 
     if column_diff:
@@ -27,6 +67,45 @@ def campbell_bozorgnia_2014(sites: pd.DataFrame) -> AmplificationArray:
 
 
 def bayless_abrahamson_2018(sites: pd.DataFrame) -> AmplificationArray:
+    """Site amplification factor based on the Bayless and Abrahamson
+    2018 effective amplitude spectra ground motion model [0] with
+    modifications[1].
+
+    Parameters
+    ----------
+    sites : pd.DataFrame
+        DataFrame of sites to calculate amplification factors for.
+        Must contain all columns in `REQUIRED_COLUMNS`, at least
+        ``vs30`` (vs30 of the site), ``vs30_sim`` (vs30 of the
+        simulation, typically 500 m/s), and ``pga`` (peak ground
+        acceleration from the simulation).
+
+    Returns
+    -------
+    AmplificationArray
+        An array of shape ``(n_stations, n_frequencies)`` where
+        ``n_frequencies`` is the number of frequencies supported by
+        the model.
+
+    Notes
+    -----
+    Implementation details are in ``src-rust/bayless_abrahamson_2018.rs``
+
+    See Also
+    --------
+    BAYLESS_ABRAHAMSON_2018_FREQUENCIES : Frequencies of the amplification values.
+
+    References
+    ----------
+    [0] Bayless, J., & Abrahamson, N. A. (2019). Summary of the BA18
+    ground‐motion model for Fourier amplitude spectra for crustal
+    earthquakes in California. Bulletin of the Seismological Society
+    of America, 109(5), 2088-2105.
+    [1] Kuncar F, Bradley BA, de la Torre CA, Rodriguez-Marek A, Zhu C,
+    Lee RL. Methods to account for shallow site effects in hybrid
+    broadband ground-motion simulations. Earthquake Spectra.
+    2025;41(2):1272-1313.
+    """
     column_diff = REQUIRED_COLUMNS - set(sites.columns)
 
     if column_diff:
